e303736d3f8b71109a0c9b9fd29517e8d1574601
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-22 22:52:07 +0530

    add delete user func

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php include('connection.php');
+
+$id = $_POST['id'];
+
+$sql = "DELETE FROM student_info WHERE id = '$id'";
+
+if ($conn->query($sql) === TRUE) {
+    $data = array(
+        'status' => true
+    );
+
+    echo json_encode($data);
+} else {
+    $data = array(
+        'status' => false,
+        "message" => "Error deleting record: . '$conn->error'"
+    );
+}
