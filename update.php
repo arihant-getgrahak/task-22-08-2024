ab1d9adeec6cd9f4b47d4b8eba1dc937c379e366
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-08-22 22:51:48 +0530

    add update user func

---
+++ update.php
@@ -0,0 +1,22 @@
+<?php include('connection.php');
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$mobile = $_POST['mobile'];
+$city = $_POST['city'];
+$id = $_POST['id'];
+
+$sql = "UPDATE student_info SET name = '$name', email = '$email', mobile = '$mobile', city = '$city' WHERE id = '$id'";
+
+if ($conn->query($sql) === TRUE) {
+   $data = array(
+       'status' => true
+   );
+
+   echo json_encode($data);
+} else {
+    $data = array(
+        'status' => false,
+        "message" => "Error updating record: . '$conn->error'"
+    );
+}
